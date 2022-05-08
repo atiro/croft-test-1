eaab02c95709b32f37376a28475793662b44a146
Richard Palmer <r.palmer@vam.ac.uk>
2022-05-08 18:36:09 +0100

    Remove non-existent file

--- docs/sw.js
+++ docs/sw.js
@@ -21,8 +21,7 @@ const RUNTIME = 'runtime';
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
-  'search.json',
-  'app.js'
+  'search.json'
 ];
 
 // The install handler takes care of precaching the resources we always need.
